7e5ac46ec62ac0b5f0e2e4f4f124fa980060a03e
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-11-23 13:56:40 +0300

    Palindrome

---
+++ Palindrome.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+func isPalindrome1(_ value: String) -> Bool
+{
+    let len = value.count / 2
+
+    for i in 0..<len
+    {
+        let start = value.index(value.startIndex, offsetBy: i)
+        let end = value.index(value.endIndex, offsetBy: (i * -1) - 1)
+        
+        if value[start] != value[end] {
+            return false
+        }
+    }
+    return true
+}
+
+print(isPalindrome1("true"))
+
+//------------------------------------------------//
+
+func isPalindrome2( _ value: String) -> Bool
+{
+    guard value.count >= 2 else {
+        return true
+    }
+
+    let end = value.index(value.endIndex, offsetBy: -1)
+
+    if value[value.startIndex] == value[end] {
+        let start = value.index(value.startIndex, offsetBy: 1)
+        return isPalindrome2(String(value[start..<end]))
+    } else {
+        return false
+    }
+}
+print(isPalindrome2("madam"))
