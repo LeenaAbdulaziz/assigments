2377fb94255b3f55d44301e86bde1855e2ea2061
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-11-29 13:39:53 +0300

    Third assignment

--- RandomArray.playground/Contents.swift
+++ RandomArray.playground/Contents.swift
@@ -30,10 +30,14 @@ print(arr)
 
 for i in 0..<arr.count{
     if arr[i]==42{
+        arr.remove(at: i) // to remove the element as requested in the problem description
         print("We found the answer to the Ultimate Question of Life, the Universe, and Everything at index \(i)")
+        break
     }
     
 }
+
+
 
 
 
